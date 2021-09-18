7d3769e1321248fc621c2a667934e26ee3372259
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-09-18 16:59:04 -0700

    - added dependencies for:   -- ViewModel   -- LiveData   -- Saved State Module for ViewModel   -- Kapt annotation processor   -- Room   -- Coroutines   -- Dagger2   -- Retrofit + GSON + OkHttp Logging Interceptor   -- Glide
    - Added gradle build processing to grab private API keys from data text file and store in build config
    - Added the sensitive data text file to .gitignore

--- app/src/main/java/com/nfragiskatos/tmdbclient_class/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/tmdbclient_class/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.nfragiskatos.tmdbclient_class
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Access api key
+//        BuildConfig.THEMOVIEDB_API_KEY
     }
 }
