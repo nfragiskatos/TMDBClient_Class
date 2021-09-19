c9f9178bc8a379800f6f92a0345ed304691e25b7
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-09-18 18:42:35 -0700

    - removed unused KnownFor.kt (was autogenerated) - Added Room's @Entity and @Primary key annotations to data classes

--- app/src/main/java/com/nfragiskatos/tmdbclient_class/data/model/tvshow/TVShow.kt
+++ app/src/main/java/com/nfragiskatos/tmdbclient_class/data/model/tvshow/TVShow.kt
@@ -1,17 +1,26 @@
 package com.nfragiskatos.tmdbclient_class.data.model.tvshow
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "popular_tv_shows")
 data class TVShow(
-    @SerializedName("first_air_date")
-    val firstAirDate: String?,
+
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
+
+    @SerializedName("first_air_date")
+    val firstAirDate: String?,
+
     @SerializedName("name")
     val name: String?,
+
     @SerializedName("overview")
     val overview: String?,
+
     @SerializedName("poster_path")
     val posterPath: String?,
 )
