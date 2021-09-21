95348070fec2a47925e027ab5331888bb7156a3e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-09-21 16:48:53 -0700

    - renamed MainActivity.kt and activity_main.xml - Added missing ktx dependency for coroutines for Room

--- app/src/main/java/com/nfragiskatos/tmdbclient_class/presentation/HomeActivity.kt
+++ app/src/main/java/com/nfragiskatos/tmdbclient_class/presentation/HomeActivity.kt
@@ -1,12 +1,13 @@
-package com.nfragiskatos.tmdbclient_class
+package com.nfragiskatos.tmdbclient_class.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nfragiskatos.tmdbclient_class.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
         // Access api key
 //        BuildConfig.THEMOVIEDB_API_KEY
